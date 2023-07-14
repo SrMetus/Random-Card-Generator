0d3850d3a7c000f82884e2645b8498461954a135
SrMetus <s.gaggero87@gmail.com>
2023-07-14 04:49:45 -0400

    Finalizando Proyecto

---
+++ src/index.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function() {
+    randomSuitCard();
+    randomNumberCard();
+});
+
+function randomSuitCard() {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let randomSuit 
+    
+    suit.forEach(item => {
+        randomSuit = Math.floor(Math.random() * 4 + 1);
+    });
+
+    //console.log(`Numero: ${randomSuit} y Pinta: ${suit[randomSuit - 1]}`);
+    let pintas = document.querySelectorAll("#mySuit");
+    pintas.forEach((item, index) => {
+        item.innerHTML = suit[randomSuit - 1];
+        let color = document.querySelectorAll("#mySuit")[index];
+        switch(randomSuit) {
+            case 1:
+                color.classList.add("diamond");
+                break;
+            case 2:
+                color.classList.add("heart");
+                break;
+            case 3:
+                color.classList.add("spade");
+                break;
+            case 4:
+                color.classList.add("club");
+                break;
+        }
+        console.log(color);
+    });
+}
+
+function randomNumberCard() {
+    let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let randomCard
+
+    card.forEach(item => {
+        randomCard = Math.floor(Math.random() * 13);
+    });
+
+    //console.log(`Numero: ${randomCard + 1} Carta: ${card[randomCard]}`);
+    let numberCard = document.querySelector("#myCard");
+    numberCard.innerHTML = card[randomCard];
+}
